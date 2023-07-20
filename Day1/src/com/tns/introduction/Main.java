6ea035f06c3c9a0677f7d48ed3ec965b9517b71e
rohit <rohitbhandare7777@gmail.com>
2023-07-20 17:21:24 +0530

    first commit

--- Day1/src/com/tns/introduction/Main.java
+++ Day1/src/com/tns/introduction/Main.java
@@ -1,13 +1,30 @@
 package com.tns.introduction;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		// primitive data types
+		byte b = 127;  // 1 byte
+		short sh = 300; // 2 bytes
+		int a = 12345; // 4 bytes
+		long l=1234_5678_9234L; // 8 bytes
+		float f = 9.1f; // 4 bytes
+		double db=34.56; // 8 bytes
+		boolean bool=true; // 1 bit
+		char ch='a'; // 2 byte
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a name: ");
+		String name=sc.nextLine();
+		
+		System.out.println(name);
 		
 		
-		System.out.println("hello world");
-		int a=34;
-		int b=4;
+		
+		
 
 	}
 
