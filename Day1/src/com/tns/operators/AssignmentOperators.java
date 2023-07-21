d930a725f3e2c370d9a7ea48a433e89cd63af6dc
rohit <rohitbhandare7777@gmail.com>
2023-07-21 13:36:34 +0530

    third commit

---
+++ Day1/src/com/tns/operators/AssignmentOperators.java
@@ -0,0 +1,17 @@
+package com.tns.operators;
+//program to demonstrate Assignment Operator
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=12;
+		int b=4;
+		
+		b*=a;
+		System.out.println(b);
+		System.out.println(a/=b); 
+
+	}
+
+}
