ceff8b051c2c5bf5accafe298d16cec28fa17c37
rohit <rohitbhandare7777@gmail.com>
2023-07-21 13:00:21 +0530

    second commit

---
+++ Day1/src/com/tns/operators/RelationalOperators.java
@@ -0,0 +1,26 @@
+package com.tns.operators;
+
+import java.util.Scanner;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a Num 1: ");
+		int a=sc.nextInt();
+
+		System.out.println("Enter a Num 2: ");
+		int b=sc.nextInt();
+		
+		System.out.println(a>b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a<b);
+	
+
+	}
+
+}
